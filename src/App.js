d771af710c95e0e3ffbc4ffa74a0c6ac2881f6cf
Abhishek <abhikharpal@gmail.com>
2023-01-27 22:22:39 +0530

    fix: modify test tags to containers

--- src/App.js
+++ src/App.js
@@ -2,26 +2,26 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
-import HomeTest from './components/routes/home';
-import AnnouncementsTest from './components/routes/announcements';
-import InformationRadiatorTest from './components/routes/informationRadiator';
-import OurTeamsTest from './components/routes/ourTeams';
+import HomeContainer from './components/routes/home';
+import AnnouncementContainer from './components/routes/announcements';
+import InformationRadiatorContainer from './components/routes/informationRadiator';
+import OurTeamsContainer from './components/routes/ourTeams';
 import PoNotesContainer from './components/routes/poNotes';
-import RefMaterialsTest from './components/routes/refMaterials';
-import TimelinesTest from './components/routes/timelines';
+import RefMaterialsContainer from './components/routes/refMaterials';
+import TimelineContainer from './components/routes/timelines';
 
 
 function App() {
   return (
 
     <Routes>
-      <Route exact path='/' element={<HomeTest />} />
-      <Route exact path='/announcements' element={<AnnouncementsTest />} />
-      <Route exact path='/information-radiator' element={<InformationRadiatorTest />} />
-      <Route exact path='/our-teams' element={<OurTeamsTest />} />
+      <Route exact path='/' element={<HomeContainer />} />
+      <Route exact path='/announcements' element={<AnnouncementContainer />} />
+      <Route exact path='/information-radiator' element={<InformationRadiatorContainer />} />
+      <Route exact path='/our-teams' element={<OurTeamsContainer />} />
       <Route exact path='/po-notes' element={<PoNotesContainer />} />
-      <Route exact path='/reference-material' element={<RefMaterialsTest />} />
-      <Route exact path='/timeline' element={<TimelinesTest />} />
+      <Route exact path='/reference-material' element={<RefMaterialsContainer />} />
+      <Route exact path='/timeline' element={<TimelineContainer />} />
     </Routes>
   );
 }
