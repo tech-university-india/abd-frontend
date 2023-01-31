5cbd0ea918510dc177e8b1109e4b3ee9e8a99ff5
Abhishek <abhikharpal@gmail.com>
2023-01-31 10:53:24 +0530

    fix: render navbar globally

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import {Box} from "@mui/material";
 
 import HomeContainer from './components/routes/home';
 import AnnouncementContainer from './components/routes/announcements';
@@ -9,19 +10,24 @@ import OurTeamsContainer from './components/routes/ourTeams';
 import PoNotesContainer from './components/routes/poNotes';
 import RefMaterialsContainer from './components/routes/refMaterials';
 import TimelineContainer from './components/routes/timelines';
+import Navbar from './components/elements/navBar';
 
 function App() {
   return (
-
-    <Routes>
-      <Route exact path='/' element={<HomeContainer />} />
-      <Route exact path='/announcements' element={<AnnouncementContainer />} />
-      <Route exact path='/information-radiators' element={<InformationRadiatorContainer />} />
-      <Route exact path='/our-teams' element={<OurTeamsContainer />} />
-      <Route exact path='/po-notes' element={<PoNotesContainer />} />
-      <Route exact path='/reference-material' element={<RefMaterialsContainer />} />
-      <Route exact path='/timelines-roadmaps' element={<TimelineContainer />} />
-    </Routes>
+    <Box>
+      <Box>
+        <Navbar/>
+      </Box>
+      <Routes>
+        <Route exact path='/' element={<HomeContainer />} />
+        <Route exact path='/announcements' element={<AnnouncementContainer />} />
+        <Route exact path='/information-radiators' element={<InformationRadiatorContainer />} />
+        <Route exact path='/our-teams' element={<OurTeamsContainer />} />
+        <Route exact path='/po-notes' element={<PoNotesContainer />} />
+        <Route exact path='/reference-material' element={<RefMaterialsContainer />} />
+        <Route exact path='/timelines-roadmaps' element={<TimelineContainer />} />
+      </Routes>
+    </Box>
   );
 }
 
