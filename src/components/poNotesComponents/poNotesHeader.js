fce11f4b73b19aecfbd47b43cf67b1cd35ff564b
samim-hossain-mondal <Samim_Hossain_Mondal@mckinsey.com>
2023-02-05 02:58:57 +0530

    (fix): refactor and structure the code

--- src/components/poNotesComponents/poNotesHeader.js
+++ src/components/poNotesComponents/poNotesHeader.js
@@ -1,62 +1,56 @@
 import React, { useState } from 'react';
-
-import { Box, AppBar, Container, InputLabel, MenuItem, FormControl, Select, Toolbar, Typography } from '@mui/material';
+import {
+  Box, AppBar, Container,
+  ThemeProvider, InputLabel,
+  MenuItem, FormControl, Select,
+  Toolbar, Typography
+} from '@mui/material';
 import FilterAltOutlinedIcon from '@mui/icons-material/FilterAltOutlined';
-
 import SearchBar from '../utilityFunctions/searchBar';
 import AddPoNotes from './addPoNotes';
+import theme from '../themes/globalTheme';
 
-// TODO get request filter from onSubmit search
-// TODO get request filter from quick filter
-// TODO post request from add Note
-
-function PoNotesHeader() {
-
+export default function PoNotesHeader() {
   const [searchQuery, setSearchQuery] = useState("");
-  const [quickFilterType, setQuickFilter] = React.useState('');
-  
+  const [quickFilterType, setQuickFilter] = useState('');
   const quickFilterHandler = (event) => {
     setQuickFilter(event.target.value);
   };
-
   return (
-    <AppBar position="static" style={{ background: 'transparent', boxShadow: 'none' }}>
-      <Container maxWidth="lg">
-        <Toolbar disableGutters>
-          <Typography
-            data-testid="poNotesIdentifier"
-            variant="h5"
-            noWrap
-            sx={{ flexGrow: 2, mr: 2, display: { xs: 'none', md: 'flex' }, fontWeight: 500, letterSpacing: '.025rem', color: '#3D3D3D;', textDecoration: 'none' }}
-          >
-            PO Notes
-          </Typography>
-          <Box sx={{ flexGrow: 0.2, display: { xs: 'none', md: 'flex' } }}>
-            <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
-          </Box>
-          <Box sx={{ flexGrow: 0.2, display: { xs: 'none', md: 'flex' } }}>
-            <FormControl sx={{ m: 1, minWidth: 200 }} size="small">
-              <InputLabel id="demo-select-small"> Quick Filters <FilterAltOutlinedIcon fontSize='small' /></InputLabel>
-              <Select
-                labelId="demo-select-small"
-                id="demo-select-small"
-                value={quickFilterType}
-                label="Quick Filter"
-                onChange={quickFilterHandler}
-              >
-                <MenuItem value="">
-                  <em>None</em>
-                </MenuItem>
-                <MenuItem value={1}>Today</MenuItem>
-                <MenuItem value={2}>Yesterday</MenuItem>
-                <MenuItem value={3}>Custom Date Range</MenuItem>
-              </Select>
-            </FormControl>
-          </Box>
-          <AddPoNotes />
-        </Toolbar>
-      </Container>
-    </AppBar >
+    <ThemeProvider theme={theme}>
+      <AppBar position="static" sx={{ background: 'transparent', boxShadow: 'none' }}>
+        <Container maxWidth="lg">
+          <Toolbar disableGutters>
+            <Typography
+              data-testid="poNotesIdentifier" color='secondary.main'
+              variant="h5" sx={{ flexGrow: 2, mr: 2, display: { xs: 'none', md: 'flex' } }}
+            >
+              PO Notes
+            </Typography>
+            <Box sx={{ flexGrow: 0.2, display: { xs: 'none', md: 'flex' } }}>
+              <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+            </Box>
+            <Box sx={{ flexGrow: 0.2, display: { xs: 'none', md: 'flex' } }}>
+              <FormControl sx={{ m: 1, minWidth: 200 }} size="small">
+                <InputLabel id="demo-select-small"> Quick Filters
+                  <FilterAltOutlinedIcon fontSize='small' />
+                </InputLabel>
+                <Select
+                  id="demo-select-small" labelId="demo-select-small"
+                  value={quickFilterType} label="Quick Filter"
+                  onChange={quickFilterHandler}
+                >
+                  <MenuItem value=""> <em>None</em> </MenuItem>
+                  <MenuItem value={1}>Today</MenuItem>
+                  <MenuItem value={2}>Yesterday</MenuItem>
+                  <MenuItem value={3}>Custom Date Range</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+            <AddPoNotes />
+          </Toolbar>
+        </Container>
+      </AppBar >
+    </ThemeProvider >
   );
 }
-export default PoNotesHeader;
