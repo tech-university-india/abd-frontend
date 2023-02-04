ea8b0efc1ee89d0ffa3e8e9b33a6425d2ca3f3b4
Abhishek <abhikharpal@gmail.com>
2023-02-05 02:08:26 +0530

    chore: used state to retrieve timeline

--- src/components/utilityFunctions/timeline.js
+++ src/components/utilityFunctions/timeline.js
@@ -1,24 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Box,Typography,List,ListItem,TextField } from '@mui/material';
 
-function Timeline(){
+function Timeline({ isSubmit, timeline, setTimeline }){
+    const handleTimeline = (e) => {
+      setTimeline(e.target.value);
+    };
     return (
         <Box>
           <Typography sx={{ fontWeight: 700, marginLeft: '20px', marginTop: '20px' }}>Timeline</Typography>
           <List>
             <ListItem>
               <TextField
+                disabled={isSubmit}
                 id="datetime-local"
                 label="Select date, time"
                 type="datetime-local"
-                defaultValue="2017-05-24T10:30"
+                defaultValue={timeline}
                 sx={{ width: 350 }}
                 InputLabelProps={{ shrink: true }}
+                onChange={handleTimeline}
               />
             </ListItem>
           </List>
         </Box>
     );
 }
+
+Timeline.propTypes = {
+  isSubmit: PropTypes.bool.isRequired,
+  timeline: PropTypes.string.isRequired,
+  setTimeline: PropTypes.func.isRequired
+};
 
 export default Timeline;
