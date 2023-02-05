39a6aa5b62724c30520334512c6241d4626cf8fd
karthik0145 <karthikshriram5111@gmail.com>
2023-02-05 18:35:47 +0530

    feat: add table for displaying agenda items

---
+++ src/components/poNotesComponents/PONotesTables/AgendaItemsTable.js
@@ -0,0 +1,39 @@
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Paper
+} from '@mui/material'
+import React from 'react'
+import Box from '@mui/material/Box';
+import { ThemeProvider } from '@mui/material/styles';
+import AgendaItemsTableHeader from './PONotesTablesHeader/AgendaItemsTableHeader'
+import PONotesTableTheme from '../../Theme/globalTheme'
+
+
+// table for the action items
+export default function ActionItemsTable() {
+  return (
+    <Box sx={{ width: '600px' }}>
+      <ThemeProvider theme={PONotesTableTheme}>
+        <TableContainer component={Paper}>
+          <Table stickyHeader aria-label='simple table'>
+            <TableHead>
+              <TableRow align='center'>
+                {/* calling the action item table header and passing count of action items in the table as props in countOfItems variable */}
+                <TableCell><AgendaItemsTableHeader countOfItems={0} /></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody >
+              <TableRow />
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </ThemeProvider>
+    </Box>
+
+  )
+}
