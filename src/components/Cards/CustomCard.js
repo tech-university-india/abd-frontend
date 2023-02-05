78f936686f2d0d5b0b51b3175ab70bcd8abbde38
kartik162000 <kartikg.goel@gmail.com>
2023-02-05 10:51:45 +0530

    fix: add eslint rule for bitwise operation

--- src/components/Cards/CustomCard.js
+++ src/components/Cards/CustomCard.js
@@ -19,15 +19,11 @@ const stringToColor = (string) => {
   let hash = 0;
   let i;
   for (i = 0; i < string.length; i += 1) {
-
-    // eslint-disable-next-line no-bitwise
     hash = string.charCodeAt(i) + ((hash << 5) - hash);
   }
   let color = '#';
 
   for (i = 0; i < 3; i += 1) {
-
-    // eslint-disable-next-line no-bitwise
     const value = (hash >> (i * 8)) & 0xff;
     color += `00${value.toString(16)}`.slice(-2);
   }
