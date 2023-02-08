6e5c2bcbccc88bbd8db1d38aa8d5bc54cf02ffe7
kartik162000 <kartikg.goel@gmail.com>
2023-02-08 15:51:40 +0530

    fix: remove bitwise operator from stringTocolor function

--- src/components/Cards/CustomCard.js
+++ src/components/Cards/CustomCard.js
@@ -1,25 +1,14 @@
 import React,{useState} from 'react'
 import { PropTypes } from 'prop-types';
 import { Box, Card, CardContent, Typography, Button, Checkbox,useTheme, styled ,Stack,Avatar} from '@mui/material';
+import stc from 'string-to-color';
 
 // import theme from './theme';
 import Status from './Status';
 import dateGetter from '../utilityFunctions/dateGetter';
 
-const stringToColor = (string) => {
-  let hash = 0;
-  let i;
-  for (i = 0; i < string.length; i += 1) {
-    hash = string.charCodeAt(i) + ((hash << 5) - hash);
-  }
-  let color = '#';
+const stringToColor = (string) => (stc(string)) 
 
-  for (i = 0; i < 3; i += 1) {
-    const value = (hash >> (i * 8)) & 0xff;
-    color += `00${value.toString(16)}`.slice(-2);
-  }
-  return color;
-}
 
 function stringAvatar(name) {
   return {
