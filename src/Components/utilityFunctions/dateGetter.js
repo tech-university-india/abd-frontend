0691e69d863fe5c32c5a39387652d745589a401e
samim-hossain-mondal <122428887+samim-hossain-mondal@users.noreply.github.com>
2023-02-11 13:33:04 +0530

    chore: delete duplicate folders/files

--- src/Components/utilityFunctions/dateGetter.js
+++
@@ -1,9 +0,0 @@
-
-const dateGetter = (timeStamp) => {
-    const date = new Date(timeStamp);
-    const month = date.toLocaleString('default', { month: 'short' });
-    const day = date.getDate();
-    const year = date.getFullYear();
-    return `${day} ${month}, ${year}`;
-}
-export default dateGetter;
