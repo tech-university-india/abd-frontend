49ce5adcc387dde2ba91af8fbfcbcadb88aaa64d
kartik162000 <kartikg.goel@gmail.com>
2023-02-13 14:45:45 +0530

    fix: name case

---
+++ src/components/utilityFunctions/DateGetter.js
@@ -0,0 +1,14 @@
+
+const dateGetter = (timeStamp,dateHolder) => {
+    const DUE_DATE = "dueDate";
+    const date = new Date(timeStamp);
+    const month = date.toLocaleString('default', { month: 'short' });
+    const day = date.getDate();
+    const year = date.getFullYear();
+    const time= date.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+    if(dateHolder===DUE_DATE)
+    return `${day} ${month}, ${year}`;
+    return `${day} ${month}, ${year} ${time}`;
+
+}
+export default dateGetter;
