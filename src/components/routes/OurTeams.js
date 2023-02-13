f9e92ee676d07cb502327620a9403e7eb84ee946
kartik162000 <kartikg.goel@gmail.com>
2023-02-13 14:43:21 +0530

    fix: change name case of files

---
+++ src/components/routes/OurTeams.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+export default function OurTeamsContainer() {
+  return (
+    <Box>
+      Welcome to Our Teams page.
+    </Box>
+  );;
+};
