cf15bf89a828d5363a0a02a0ccc2d7f069d0cf3c
Bharath Raj <BharathRajgngowdara@gmail.com>
2023-02-14 00:47:12 +0530

    feat: add placeholder constant to enums file

--- src/components/utilityFunctions/Enums.js
+++ src/components/utilityFunctions/Enums.js
@@ -15,8 +15,14 @@ const HEADINGS = {
   'Agenda Items': 'agenda_item',
   'Key Decisions': 'key_decision',
 };
+const PLACEHOLDER = {
+  'ACTION_ITEM': 'Example: PO is to get the marketing approvals for the Payment screen text content by Monday so that we are prepared for our next sprint.',
+  'KEY_DECISION': 'Example: The client suggested to use Stripe for payment integration as they already have corporate subscription.',
+  'AGENDA_ITEM': 'Example: Which cloud platform are we choosing to host our app? Our Client team wants to know by this week.'
+}
 module.exports = {
   STATUS,
   TYPE,
-  HEADINGS
+  HEADINGS,
+  PLACEHOLDER
 };
