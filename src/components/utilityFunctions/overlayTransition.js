68277127589c239b0308b7ee3ff5e03f0f7cff67
Kartik Goel <59787066+kartik162000@users.noreply.github.com>
2023-02-13 14:55:16 +0530

    Delete overlayTransition.js

--- src/components/utilityFunctions/overlayTransition.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Slide } from '@mui/material';
-
-const Transition = React.forwardRef(
-  (props, ref) => <Slide direction="up" ref={ref} {...props} />
-);
-
-export default Transition;
