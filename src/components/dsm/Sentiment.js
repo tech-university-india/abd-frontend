06571662c436e572b66692949ee6b9a1d34963f7
Abhishek <53441820+Abhishek-Kharpal@users.noreply.github.com>
2023-02-16 21:10:46 +0530

    Chore/dsm-layout (#19)
    * chore: add dependency for masonry
    
    * chore: add layout for dsm page
    
    * fix: full screen icon on celebration board
    
    * fix: width of components was adjusted
    
    * fix: rename dsmComponents to dsm
    
    * fix: delete unnecessary spaces

---
+++ src/components/dsm/Sentiment.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+export default function Sentiment() {
+  return (
+    <Box sx={{minHeight:'104px'}}>
+      <h1>Sentiment</h1>
+    </Box>
+  );
+};
