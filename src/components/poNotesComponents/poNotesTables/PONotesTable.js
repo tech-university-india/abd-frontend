0c9969db8fe42716c027518955cc657710314ded
Bharath Raj <bharathrajgngowdara@gmail.com>
2023-02-16 20:57:19 +0530

    fix: remove internal ThemeProvider

--- src/components/poNotesComponents/poNotesTables/PONotesTable.js
+++ src/components/poNotesComponents/poNotesTables/PONotesTable.js
@@ -1,11 +1,9 @@
 import { TableContainer, Table, TableHead, TableBody, TableRow, TableCell, Paper, CircularProgress } from '@mui/material'
 import React from 'react'
 import Box from '@mui/material/Box';
-import { ThemeProvider } from '@mui/material/styles';
 import PropTypes from 'prop-types';
 import { useQuery } from 'react-query';
 import PONotsTableHeader from './poNotesTablesHeader/PONotesTableHeader';
-import PONotesTableTheme from '../../theme/GlobalTheme';
 import CardLayout from '../../cards/CardLayout';
 import { DOMAIN } from '../../../config';
 import { HEADINGS } from "../../utilityFunctions/Enums"
@@ -47,29 +45,27 @@ export default function PONotesTable(props) {
     const countOfItems = data.length;
     return (
         <Box sx={{ width: '600px' }}>
-            <ThemeProvider theme={PONotesTableTheme}>
-                <TableContainer component={Paper}>
-                    <Table align="center" textAlign="center" stickyHeader aria-label='simple table'>
-                        <TableHead>
-                            <TableRow align='center'>
-                                {/* calling the action item table header and passing count of action items in the table as props in countOfItems variable */}
-                                <TableCell>
-                                    {/* Information regarding each PO Notes type(Action Items, Key decisions and Agenda Items are passed as props to table header) */}
-                                    <PONotsTableHeader heading={heading}
-                                        definition={definition}
-                                        accessibilityInformation={accessibilityInformation}
-                                        countOfItems={countOfItems} />
-                                </TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody >
-                            <TableRow>
-                                {/* Data from get Api call using query params is passed to cardlayout for displaying it in cards */}
-                                <CardLayout checkBox={checkBox} type={HEADINGS[heading]} data={data} /> </TableRow>
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            </ThemeProvider>
+            <TableContainer component={Paper}>
+                <Table align="center" textAlign="center" stickyHeader aria-label='simple table'>
+                    <TableHead>
+                        <TableRow align='center'>
+                            {/* calling the action item table header and passing count of action items in the table as props in countOfItems variable */}
+                            <TableCell>
+                                {/* Information regarding each PO Notes type(Action Items, Key decisions and Agenda Items are passed as props to table header) */}
+                                <PONotsTableHeader heading={heading}
+                                    definition={definition}
+                                    accessibilityInformation={accessibilityInformation}
+                                    countOfItems={countOfItems} />
+                            </TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody >
+                        <TableRow>
+                            {/* Data from get Api call using query params is passed to cardlayout for displaying it in cards */}
+                            <CardLayout checkBox={checkBox} type={HEADINGS[heading]} data={data} /> </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </Box>
     )
 }
