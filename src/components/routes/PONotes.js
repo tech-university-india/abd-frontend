3413cab3bd29e86cf6c76d09d3db1d943c66f25a
Bharath Raj <BharathRajgngowdara@gmail.com>
2023-02-16 15:52:16 +0530

    fix: move theme provided to global index

--- src/components/routes/PONotes.js
+++ src/components/routes/PONotes.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import { Box, ThemeProvider } from '@mui/material';
+import { Box } from '@mui/material';
 import PONotesBody from '../poNotesComponents/PONotesBody';
 import PONotesHeader from '../poNotesComponents/PONotesHeader';
-import theme from '../theme/GlobalTheme';
 
 export default function PONotesContainer() {
 
   return (
-    <ThemeProvider theme={theme}>
-      <Box>
-        <Box sx={{ m: -2, p: 4 }}>
-          <PONotesHeader />
-        </Box>
-        <Box > <PONotesBody /> </Box>
+    <Box>
+      <Box sx={{ m: -2, p: 4 }}>
+        <PONotesHeader />
       </Box>
-    </ThemeProvider>
-
+      <Box > <PONotesBody /> </Box>
+    </Box>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import ThemeProvider from '@mui/material/styles/ThemeProvider';
 import App from './App';
 import { ErrorProvider } from './components/contexts/ErrorContext';
+import theme from "./components/theme/GlobalTheme";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <ErrorProvider>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </ErrorProvider>
+    <ThemeProvider theme={theme}>
+        <ErrorProvider>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </ErrorProvider>
+    </ThemeProvider>
 );
 
