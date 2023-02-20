7a4dfd6b19764d2db84c81f49276ae2b107eec4b
Ritik Rajdev <ritik_rajdev@mckinsey.com>
2023-02-21 01:34:38 +0530

    feat: create generic modal to take input

--- src/components/dsm/Announcements.js
+++ src/components/dsm/Announcements.js
@@ -1,13 +1,25 @@
-import React, { useContext } from 'react';
-import { Grid, Accordion, AccordionSummary, AccordionDetails, Typography } from '@mui/material';
+import React, { useContext, useState } from 'react';
+import { Grid, Accordion, AccordionSummary, AccordionDetails, Typography, IconButton, Dialog } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { AddCircle as AddCircleIcon } from '@mui/icons-material';
 import { DSMBodyLayoutContext } from "../contexts/DSMBodyLayoutContext"
+import GenericInputModal from '../elements/dsm/GenericInputModal';
 
 export default function Announcements() {
   const { gridHeightState, dispatchGridHeight } = useContext(DSMBodyLayoutContext)
   const handleExpandAnnouncements = () => {
     dispatchGridHeight({ type: "ANNOUNCEMENT" })
   };
+
+  const [openModal, setOpenAddModal] = useState(false);
+  const handleAddButtonClick = () => {
+    setOpenAddModal(!openModal);
+    dispatchGridHeight({ type: "ANNOUNCEMENT" });
+  }
+
+  const handleModalClose = () => {
+    setOpenAddModal(false);
+  }
 
   return (
     <Grid item height={gridHeightState.announcement.height} >
@@ -18,10 +30,26 @@ export default function Announcements() {
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel4a-content"
           id="panel4a-header"
+          sx={{
+            '.MuiAccordionSummary-content': {
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              width: "100%",
+            }
+          }}
         >
-          {/* All Content/Development of Announcements HEADER goes here */}
           <Typography variant="dsmSubMain">Announcements</Typography>
+          <IconButton onClick={handleAddButtonClick}>
+            <AddCircleIcon color="primary" />
+          </IconButton>
         </AccordionSummary>
+        <Dialog
+          open={openModal}
+          onClose={handleModalClose}
+        >
+          <GenericInputModal title='Announcement' onCloseButtonClick={handleModalClose} primaryButtonText='Save' />
+        </Dialog>
         <AccordionDetails>
           {/* All Content/Development of Announcements BODY goes here */}
           <Typography>
