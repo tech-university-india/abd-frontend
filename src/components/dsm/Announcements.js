d64ad90661d9432644582eb46b4390c243c4438b
Ritik Rajdev <ritik_rajdev@mckinsey.com>
2023-02-21 02:46:47 +0530

    feat: add read functionality for each announcement

--- src/components/dsm/Announcements.js
+++ src/components/dsm/Announcements.js
@@ -1,9 +1,10 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Grid, Accordion, AccordionSummary, AccordionDetails, Typography, IconButton, Dialog } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { AddCircle as AddCircleIcon } from '@mui/icons-material';
 import { DSMBodyLayoutContext } from "../contexts/DSMBodyLayoutContext"
 import GenericInputModal from '../elements/dsm/GenericInputModal';
+import ChatContainer from '../elements/dsm/ChatContainer';
 
 export default function Announcements() {
   const { gridHeightState, dispatchGridHeight } = useContext(DSMBodyLayoutContext)
@@ -11,7 +12,7 @@ export default function Announcements() {
     dispatchGridHeight({ type: "ANNOUNCEMENT" })
   };
 
-
+  const [announcements, setAnnouncements] = useState([]);
   const [openModal, setOpenAddModal] = useState(false);
   const handleAddButtonClick = () => {
     setOpenAddModal(!openModal);
@@ -21,6 +22,33 @@ export default function Announcements() {
   const handleModalClose = () => {
     setOpenAddModal(false);
   }
+
+  // TODO: integrate with backend
+  // OPTIMIZE: Is backend for announcements paginated ?
+  const getAnnouncements = async () => [
+      {
+        "announcementId": 0,
+        "author": "Ritik Rajdev",
+        "content": "This is a test announcement",
+        "createdAt": "2023-02-20T20:35:20.327Z"
+      },
+      {
+        "announcementId": 1,
+        "author": "Ritik Rajdev",
+        "content": "This is a test announcement",
+        "createdAt": "2023-02-20T20:35:20.327Z"
+      }
+    ]
+
+  useEffect(() => {
+    getAnnouncements().then((_announcements) => {
+      setAnnouncements(_announcements);
+    })
+  }, [])
+
+  const handleChatClick = (announcement) => {
+    console.log(announcement);
+  };
 
   return (
     <Grid item height={gridHeightState.announcement.height} >
@@ -63,12 +91,22 @@ export default function Announcements() {
           />
           
         </Dialog>
-        <AccordionDetails>
-          {/* All Content/Development of Announcements BODY goes here */}
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
+        <AccordionDetails sx={{
+          display: "flex",
+          flexDirection: "column",
+          padding: "16px",
+          gap: "16px",
+        }}>
+          {announcements.map((announcement) => (
+            <ChatContainer
+              key={announcement.announcementId}
+              name={announcement.author}
+              content={announcement.content}
+              date={new Date(announcement.createdAt)}
+              onClick={() => handleChatClick(announcement)}
+            />
+          ))}
+
         </AccordionDetails>
       </Accordion>
     </ Grid >
