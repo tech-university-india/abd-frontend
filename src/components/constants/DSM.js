a69179cac016af192e83109cfecb142eb959a4de
Bharath Raj <bharathrajgngowdara@gmail.com>
2023-03-05 17:00:37 +0530

    feat: add constants for DSM

--- src/components/constants/DSM.js
+++ src/components/constants/DSM.js
@@ -3,8 +3,30 @@ const celebrationType = {
   IMPEDIMENT: "IMPEDIMENT"
 }
 
-const requestType = {
-
+const celebrationPlaceholder = {
+  CELEBRATION: "Example: Thank you @GC for helping out on Payment integration yesterday.",
+  IMPEDIMENT: "Example: We should make sure designs are getting marketing sign-off before given for development."
 }
 
-export { celebrationType, requestType }
+const instructions = {
+  CELEBRATION: {
+    header: "Sharing is Caring",
+    points: [
+      "Thank your team members to taking an extra step to help you on something.",
+      "Celebrate your small achievements."
+    ]
+  },
+  IMPEDIMENT: {
+    header: "Act wisely",
+    points: [
+      "Try not to be personal.",
+      "Seek attention to the core issue by highlighting the impact."
+    ]
+  }
+}
+
+const celebrationTypes = [
+  "CELEBRATION", "IMPEDIMENT"
+]
+
+export { celebrationType, celebrationPlaceholder, instructions, celebrationTypes }
